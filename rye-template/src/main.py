9fd120a828e33ee1d5b20eba4bf43baf647a536e
Shibuya Keita <shibuya.keita@cloud-ace.jp>
2024-11-05 20:21:29 +0900

    mod: #4 rye-template のパッケージをアップデート

--- rye-template/src/main.py
+++ rye-template/src/main.py
@@ -5,4 +5,9 @@ from src.variables import BASE_URI
 
 app = FastAPI()
 
+# ルートへのアクセスで Hello, world! を返す
+@app.get("/")
+def read_root():
+    return "Hello, world"
+
 app.include_router(health.router, prefix=BASE_URI, tags=["health"])
